07a547508317ca133bb325b469b9f6dd4b0e2d14
nitya1912 <137202773+nitya1912@users.noreply.github.com>
2023-08-20 22:34:22 +0530

    Add files via upload

---
+++ DoWhileLoop(2).cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int a,i=1;
+  cout <<"Enter a number: "<<endl;
+  cin>>a;
+
+  do
+  {
+    cout<<i<<"X"<<a<<" = "<<i+a<<" "<<endl;
+    i++;
+  }
+  while(i<=10);
+  return 0;
+}
